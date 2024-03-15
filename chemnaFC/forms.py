96f7e36d4d18587f4f8e60e2529648de21b4425d
biluyb <biluyb338@gmail.com>
2024-03-15 23:25:16 +0300

    add java script

--- chemnaFC/forms.py
+++ chemnaFC/forms.py
@@ -1,32 +1,32 @@
-# from django import forms
-# from .models import Fans,FanPicture
-# from django.contrib.auth.models import User
-# from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .models import Fans,FanPicture
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
-# class FanForm(forms.ModelForm):
-#     confirm_password = forms.CharField(widget=forms.PasswordInput)
-#     class Meta:
-#         model= Fans
-#         fields="__all__"
+class FanForm(forms.ModelForm):
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+    class Meta:
+        model= Fans
+        fields="__all__"
 
-#         labels ={
-#            "name":"Your Name",
-#            "email":"Your Email Address"
-#         }
-#         error_message={
-#             "name": {
-#                 "required":"You Must Enter Your Name",
-#                 "max_length":"Your Name is Too Long"
-#             }
-#         }
-# class FansPictureForm(forms.ModelForm):
-#     class Meta:
-#         model=FanPicture
-#         fields="__all__"
-# class RegisterForm(UserCreationForm):
-#     # email = forms.EmailField(required=True)
+        labels ={
+           "name":"Your Name",
+           "email":"Your Email Address"
+        }
+        error_message={
+            "name": {
+                "required":"You Must Enter Your Name",
+                "max_length":"Your Name is Too Long"
+            }
+        }
+class FansPictureForm(forms.ModelForm):
+    class Meta:
+        model=FanPicture
+        fields="__all__"
+class RegisterForm(UserCreationForm):
+    # email = forms.EmailField(required=True)
 
-#     class Meta:
-#         model=User
-#         fields =["username","email", "password1","password2"]
-#         # fields="__all__"
+    class Meta:
+        model=User
+        fields =["username","email", "password1","password2"]
+        # fields="__all__"
