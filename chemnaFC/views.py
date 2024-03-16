f7e43b59da201354bb6e3a165f8ccc2de63ad32e
biluyb <biluyb338@gmail.com>
2024-03-16 09:07:42 +0300

    login required

--- chemnaFC/views.py
+++ chemnaFC/views.py
@@ -15,7 +15,7 @@ from django.contrib.auth import  authenticate, login,logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 # Create your views here.
-
+@login_required(login_url="login")
 def index(request):
     return render(request , "chemnaFC/index.html")
 
