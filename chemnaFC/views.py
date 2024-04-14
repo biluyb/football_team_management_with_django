09dc122655989f01b4d8b1fcca10086a3fef558b
biluyb <biluyb338@gmail.com>
2024-04-14 18:02:14 +0300

    start internalization

--- chemnaFC/views.py
+++ chemnaFC/views.py
@@ -30,7 +30,7 @@ from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes
 from django.template import engines
-
+from django.utils.translation import gettext as _
 # Create your views here.
 def index(request):
     return render(request , "chemnaFC/index.html")
